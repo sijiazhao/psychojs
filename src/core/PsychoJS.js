6f21b480d663c5fc677cdbfa9b5aa20ba001d545
Sijia Zhao <sijiazhao@users.noreply.github.com>
2022-08-17 16:48:08 +0100

    Sijia modifications
    **PsychoJS.js**
    
    - debug = false
    - collectIP = true
    - logging levels: ALL / WARN
    - use https://ipapi.co/json for IP data
    - do not throw error if _getParticipantIPInfo fails
    - check completion/cancellation URL for empty string
    - change quit dialog text to say saving data
    
    **GUI.js**
    
    DlgFromDict:
    - no logo if logoURL = undefined
    - remove close button on dialog
    - ensure logoUrl is not empty string
    - only show asterisk reminder if there are fields with asterisks
    - more descriptive dropdown prompt
    - add to _setRequiredKeys if input already contains text, e.g. from query string or default value
    - remove Cancel button
    - rename Ok button to Continue
    - remove Cancel callbacks
    - remove Close callbacks
    
    dialog:
    - button text: uppercase OK
    
    _onKeyChange:
    - use event.target.id so that id can be added before element exists
    
    **ServerManager.js**
    
    - remove console logging
    
    **Window.js**
    
    - remove console logging

--- src/core/PsychoJS.js
+++ src/core/PsychoJS.js
@@ -112,15 +112,17 @@ export class PsychoJS
 	 * @param {boolean} [options.debug= true] whether to log debug information in the browser console
 	 * @param {boolean} [options.collectIP= false] whether to collect the IP information of the participant
 	 */
+	// [sijia] Changed defaults: debug = false; collectIP = true
 	constructor({
-		debug = true,
-		collectIP = false,
+		debug = false,
+		collectIP = true,
 		hosts = [],
 		topLevelStatus = true,
 	} = {})
 	{
 		// logging:
-		this._logger = new Logger(this, (debug) ? log4javascript.Level.DEBUG : log4javascript.Level.INFO);
+		// [sijia] Logging levels: ALL / WARN
+		this._logger = new Logger(this, (debug) ? log4javascript.Level.ALL : log4javascript.Level.WARN);
 		this._captureErrors();
 
 		// detect the browser:
@@ -482,8 +484,9 @@ export class PsychoJS
 			}
 
 			// save the results and the logs of the experiment:
+      // [sijia] Change change text to a saving data message
 			this.gui.finishDialog({
-				text: "Terminating the experiment. Please wait a few moments...",
+				text: "<p><b>Saving your data</b></p>Do not close your browser or leave this page",
 				nbSteps: 2 + ((isServerEnv) ? 1 : 0)
 			});
 
@@ -526,11 +529,12 @@ export class PsychoJS
 					this._window.closeFullScreen();
 
 					// redirect if redirection URLs have been provided:
-					if (isCompleted && typeof self._completionUrl !== "undefined")
+					// [sijia] Check completion/cancellation URL are not empty
+					if (isCompleted && self._completionUrl)
 					{
 						window.location = self._completionUrl;
 					}
-					else if (!isCompleted && typeof self._cancellationUrl !== "undefined")
+					else if (!isCompleted && self._cancellationUrl)
 					{
 						window.location = self._cancellationUrl;
 					}
@@ -666,7 +670,8 @@ export class PsychoJS
 		this._IP = {};
 		try
 		{
-			const url = "http://www.geoplugin.net/json.gp";
+			// [sijia] Use https://ipapi.co/json for IP data
+			const url = "https://ipapi.co/json";
 			const response = await fetch(url, {
 				method: "GET",
 				mode: "cors",
@@ -679,20 +684,22 @@ export class PsychoJS
 			{
 				throw `unable to obtain the IP of the participant: ${response.statusText}`;
 			}
-			const geoData = await response.json();
-
-			this._IP = {
-				IP: geoData.geoplugin_request,
-				country: geoData.geoplugin_countryName,
-				latitude: geoData.geoplugin_latitude,
-				longitude: geoData.geoplugin_longitude,
-			};
+			// const geoData = await response.json();
+			//
+			// this._IP = {
+			// 	IP: geoData.geoplugin_request,
+			// 	country: geoData.geoplugin_countryName,
+			// 	latitude: geoData.geoplugin_latitude,
+			// 	longitude: geoData.geoplugin_longitude,
+			// };
+			this._IP = await response.json();
 			this.logger.debug("IP information of the participant: " + util.toString(this._IP));
 		}
 		catch (error)
 		{
 			// throw { ...response, error };
-			throw Object.assign(response, { error });
+			// [sijia] Do not throw error if _getParticipantIPInfo fails
+			// throw Object.assign(response, { error });
 		}
 	}
 
--- src/core/ServerManager.js
+++ src/core/ServerManager.js
@@ -576,7 +576,8 @@ export class ServerManager extends PsychObject
 		}
 		catch (error)
 		{
-			console.error("error", error);
+			// [sijia] Remove console logging
+			// console.log("error", error);
 			throw Object.assign(response, { error });
 			// throw { ...response, error: error };
 		}
